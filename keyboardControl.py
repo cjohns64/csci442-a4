850952ef87181948431964febf537a3e0597ead0
John Lambrecht <jclambrecht97@msn.com>
2019-04-30 08:14:03 -0600

    testing shoulder

--- keyboardControl.py
+++ keyboardControl.py
@@ -79,13 +79,14 @@ class KeyControl():
         elif key.keycode == 53:
             self.elbow+=200
             #self.shoulder_side-=200
-            print(self.elbow)
-            #print(self.shoulder)
+            #print(self.elbow)
+            print(self.shoulder)
             #self.hand +=200
             #print(self.hand)
             #self.tango.setTarget(HAND, self.hand)
             #self.tango.setTarget(SHOULDER_SIDE, self.shoulder_side)
-            self.tango.setTarget(ELBOW, self.elbow)
+            #self.tango.setTarget(ELBOW, self.elbow)
+            self.tango.setTarget(SHOULDER,self.shoulder)
    
 #    def elbow(self, key):
 #        print(key.keycode)
